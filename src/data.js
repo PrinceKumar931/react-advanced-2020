2efce27e3b8bc2cdc8a9933ee0894655d18f4bca
Prince Kumar <pk754728@gmail.com>
2022-12-21 19:33:13 +0530

    initial commit

--- src/data.js
+++ src/data.js
@@ -1,6 +1,6 @@
 export const data = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
-  { id: 4, name: 'anna' },
+  { id: 1, name: 'Prince' },
+  { id: 2, name: 'Kriti' },
+  { id: 3, name: 'Krishanveer' },
+  { id: 4, name: 'Hardik' },
 ];
--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,40 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [ person, setPerson ]= useState({
+    name: 'Prince',
+    age: 21,
+    message: 'world is fucked'
+  });
+
+  const [name, setName] = useState('Prince');
+  const [age, setAge] = useState(21);
+  const [message, setMessage] = useState('World is Fucked');
+
+
+  const changeMessage = () => {
+    // setPerson({ ...person, message: 'hello world' });
+    setMessage('hello world')
+  }
+
+  // return (
+  //   <>
+  //   <h3>{person.name}</h3>
+  //   <h3>{person.age}</h3>
+  //   <h3>{person.message}</h3>
+  //   <button className='btn' onClick={changeMessage}>Change Message</button>
+
+  //   </>
+  // );
+  return (
+    <>
+    <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
+    <button className='btn' onClick={changeMessage}>Change Message</button>
+
+    </>
+  );
 };
 
 export default UseStateObject;
